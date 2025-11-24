68afe2cbe14ed18643f191f2b5dce7be70788dcd
martinoalanza03 <lanzamartino3@gmail.com>
2025-11-24 12:33:11 +0100

    feat: scaffolding header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
+import "../style/Header.css";
 function Header() {
+    return (<header className="flex flex-row justify-center">
+        <nav className="py-4 container">
+            <div class="flex items-center justify-between">
+                <div></div>
+                <div>ciao</div>
 
-
+            </div>
+        </nav>
+    </header>)
 }
 
 export default Header

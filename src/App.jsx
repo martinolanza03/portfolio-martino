74579d899f43d204aca7a05174d6b8c282694687
martinoalanza03 <lanzamartino3@gmail.com>
2025-11-25 13:23:58 +0100

    feat: create section WorkExperience

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header"
 import Me from "./components/Me"
 import WorkExperience from "./components/WorkExperience"
+import Experience from "./data/Experience.js"
 
 function App() {
 
   return (<>
     <Header />
     <Me />
-    <WorkExperience />
+    <WorkExperience works={Experience} />
   </>)
 
 }

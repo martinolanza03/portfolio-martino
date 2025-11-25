8d8a7713665991a20df236fb4c063d10495be7e2
martinoalanza03 <lanzamartino3@gmail.com>
2025-11-25 11:49:33 +0100

    feat: add margin to section for center the section, and margin to left and right for center the text

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -4,12 +4,12 @@ function Me() {
 
     return (<>
 
-        <section className="p-20 flex flex-col container items-center">
+        <section className="p-20 flex flex-col container items-center mx-auto">
             <p className="text-gray-400 text-xl mb-4">Ciao! Sono</p>
             <p className="text-blue-400 text-2xl mb-6">Martino Lanza</p>
             <p className="text-gray-400 text-xl mb-4">Sono un <span className="text-white">Fullstack Web Developer</span></p>
             <p className="text-gray-400 text-xl mb-8">Potete scaricare qui il mio <a href={cvPath} download="Martino_Lanza_Curriculum.pdf" className="text-blue-400 hover:text-blue-500 underline">Curriculum</a></p>
-            <p className="text-gray-400 text-xl text-center">Benvenuti nel mio spazio digitale! Ho una passione smisurata per la creazione di soluzioni software robuste e funzionali. Il mio cuore batte forte per il backend, ma ho una solida comprensione dell'intero stack di sviluppo.</p>
+            <p className="text-gray-400 text-xl text-center mx-80">Benvenuti nel mio spazio digitale! Ho una passione smisurata per la creazione di soluzioni software robuste e funzionali. Il mio cuore batte forte per il backend, ma ho una solida comprensione dell'intero stack di sviluppo.</p>
         </section>
 
     </>)

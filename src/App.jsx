9e13781cf72be7ad95f4b09cb3ff17b85faf1670
martinoalanza03 <lanzamartino3@gmail.com>
2025-11-25 11:58:48 +0100

    feat: import WorkExperience

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Me from "./components/Me"
+import WorkExperience from "./components/WorkExperience"
 
 function App() {
 
   return (<>
     <Header />
     <Me />
+    <WorkExperience />
   </>)
 
 }

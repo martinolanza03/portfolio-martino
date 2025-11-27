794e6764d44cc60a1a9a7706407e06c0f1d9244a
martinoalanza03 <lanzamartino3@gmail.com>
2025-11-27 11:58:01 +0100

    feat: add projectsdata

---
+++ src/data/ProjectsData.js
@@ -0,0 +1,29 @@
+const projectsdata = [
+    {
+        id: 1,
+        title: "URBN WAY",
+        description: "URBAN WAY è un e-commerce di moda moderno sviluppato come progetto finale del corso Boolean. La piattaforma permette agli utenti di esplorare prodotti fashion, gestire un carrello dinamico e completare acquisti in modo sicuro. Realizzato in team con 4 colleghi, il progetto include un backend robusto e un frontend responsive che garantisce un'esperienza d'acquisto fluida e intuitiva.",
+        image: "src/assets/projects/ecommerce.jpg",
+        technologies: ["React", "Node.js", "Vite", "JavaScript", "Bootstrap", "MySQL"],
+        githubUrl: "https://github.com/martinolanza03/fullstack-project-final"
+    },
+    {
+        id: 2,
+        title: "BooRoad",
+        description: "Booroad è una piattaforma web innovativa dedicata agli amanti dei viaggi, sviluppata come progetto di corso da 6 studenti appassionati di Boolean. Il sito si propone come punto di riferimento per viaggiatori alla ricerca di esperienze autentiche e itinerari personalizzati. ",
+        image: "src/assets/projects/booroad.jpg",
+        technologies: ["React", "Vite", "CSS", "Bootstrap"],
+        githubUrl: "https://github.com/tuo-username/task-app",
+    },
+    {
+        id: 3,
+        title: "Dc Comics",
+        description: "Questo sito vetrina dedicato a DC Comics è stato sviluppato come progetto pratico durante il mio corso di formazione con Boolean, con l'obiettivo di applicare le competenze frontend acquisite.",
+        image: "src/assets/projects/dc_comics.jpg",
+        technologies: ["React", "Vite", "CSS"],
+        githubUrl: "https://github.com/tuo-username/task-app",
+    },
+
+];
+
+export default projectsdata;

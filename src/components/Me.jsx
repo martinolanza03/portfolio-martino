13ee9566a2862ce8ad159338f7d540cea439a08f
martinoalanza03 <lanzamartino3@gmail.com>
2025-12-04 09:47:09 +0100

    feat: center the paragraph

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -3,13 +3,31 @@ function Me() {
     const cvPath = "src/assets/pdf/cv_martino.pdf";
 
     return (<>
+        <section className="p-20 w-full mx-auto">
 
-        <section className="p-20 flex flex-col container items-center mx-auto">
-            <p className="text-gray-400 text-xl mb-4">Ciao! Sono</p>
-            <p className="text-blue-400 text-2xl mb-6">Martino Lanza</p>
-            <p className="text-gray-400 text-xl mb-4">Sono uno <span className="text-white"> Junior Fullstack Web Developer</span></p>
-            <p className="text-gray-400 text-xl mb-8">Potete scaricare qui il mio <a href={cvPath} download="Martino_Lanza_Curriculum.pdf" className="text-red-400 hover:text-red-500 underline">Curriculum</a></p>
-            <p className="text-gray-400 text-xl text-center mx-80">Benvenuti nel mio spazio digitale! Ho una passione smisurata per la creazione di soluzioni software robuste e funzionali. Il mio cuore batte forte per il backend, ma ho una solida comprensione dell'intero stack di sviluppo.</p>
+            <p className="text-gray-400 text-xl mb-4 text-center">Ciao! Sono</p>
+
+            <p className="text-blue-400 text-2xl mb-6 text-center">Martino Lanza</p>
+
+            <p className="text-gray-400 text-xl mb-4 text-center">
+                Sono uno <span className="text-white"> Junior Fullstack Web Developer</span>
+            </p>
+
+            <p className="text-gray-400 text-xl mb-8 text-center">
+                Potete scaricare qui il mio
+                <a
+                    href={cvPath}
+                    download="Martino_Lanza_Curriculum.pdf"
+                    className="text-red-400 hover:text-red-500 underline"
+                >
+                    Curriculum
+                </a>
+            </p>
+
+            <p className="text-gray-400 text-xl text-center max-w-xl mx-auto px-4">
+                Benvenuti nel mio spazio digitale! Ho una passione smisurata per la creazione di soluzioni software robuste e funzionali. Il mio cuore batte forte per il backend, ma ho una solida comprensione dell'intero stack di sviluppo.
+            </p>
+
         </section>
 
     </>)
